2f91ed9d047e9a482ed450b52bea977979574899
Nimira43 <n.rabone72@aol.com>
2024-12-08 14:45:23 +0000

    Runs Rust program from with Python script

--- python/main.py
+++ python/main.py
@@ -3,3 +3,9 @@ import subprocess
 
 with open('../js/data.json', 'r') as f:
   data = json.load(f)
+
+# Pass data to Rust (serialise as JSON string)
+rust_process = subprocess.Popen(
+  ['cargo', 'run'], stdin = subprocess.PIPE, stdout = subprocess.PIPE
+)
+rust_output, _ = rust_process.communicate(input = json.dumps(data).encode())
