90f9d1e401e5505d6c6287852db41109f3f81998
Nimira43 <n.rabone72@aol.com>
2024-12-08 15:13:31 +0000

    Parse JSON input

--- rust_pipeline/src/main.rs
+++ rust_pipeline/src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).expect("Failed to read input");
 
-    // 
+    // Parse JSON input
+    let data: Value = serde_json::from_str(&input).expect("Failed to parse JSON");
 }
