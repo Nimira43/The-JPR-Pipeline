d5805b91c6eb4544c5f33a18870fc2d1004d95d9
Nimira43 <n.rabone72@aol.com>
2024-12-08 15:09:36 +0000

    Brought in a .gitignore for the Rust folder. Also importing io module, json macro and Value type. Initialise a new empty string called input and then read the input from the standard input stream into the input variable.

--- rust_pipeline/src/main.rs
+++ rust_pipeline/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Read, Write};
+use serde_json::json;
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    // Read input from stdin
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).expect("Failed to read input");
+
+    // 
 }
